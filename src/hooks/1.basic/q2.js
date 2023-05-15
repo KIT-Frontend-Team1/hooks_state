f2887f704f82e55fc024eb57288ee1e6b5ac266d
ooherin <555ohr@naver.com>
2023-05-15 16:22:06 +0900

    refactoring: state 플로우 1차 리팩토링

--- src/hooks/1.basic/q2.js
+++ src/hooks/1.basic/q2.js
@@ -4,7 +4,6 @@ function Q2() {
   const arr = useRef([]);
   const [forceRender, setForceRender] = useState(false);
   const Input = useRef();
-  const [text, setText] = useState("");
   const [showList, setShowList] = useState(false);
 
   //list에 추가하는 함수
@@ -23,6 +22,12 @@ function Q2() {
   const onSubmit = () => {
     setShowList(true);
   };
+
+  //추가버튼 누를때 forceRender가 변경되는 것을 이용해서, 해당 forceRender
+  //가 변경되면 ShowList가 false상태가 되어 리스트가 보이지 않게 함.
+  useEffect(() => {
+    setShowList(false);
+  }, [forceRender]);
 
   //2-2
   //색상을 바꾸는 함수
@@ -68,34 +73,95 @@ function Q2() {
 }
 export default Q2;
 
-/* 
-    문제2
+//리팩토링 전
+// import { useState, useRef, useEffect } from "react";
 
-    2-1)
-        useRef에 관련한 문제입니다.
+// function Q2() {
+//   const arr = useRef([]);
+//   const [forceRender, setForceRender] = useState(false);
+//   const Input = useRef();
+//   const [text, setText] = useState();
+//   console.log("render text", text);
 
-        추가 버튼을 누르면 input에 있던 value는 배열 arr에 추가됩니다.
-        그러나, 추가 버튼을 누를 때마다 강제 랜더링 상태가 무조건 적으로 업데이트 됩니다.
+//   const onAddList = () => {
+//     setForceRender((prev) => !prev);
+//     if (Input.current.value === "") return;
+//     arr.current.push(Input.current.value);
+//     Input.current.value = "";
+//   };
 
-        이러한 상황에서 제출버튼을 누르면
+//   //제출버튼을 누르면 arr에 있던 내용이 list에서 렌더링 되어야 함.
+//   const onSubmit = () => {
+//     if (arr.current.lenght === 0) return;
+//     setText(arr.current.map((item) => <li key={Math.random()}>{item}</li>)); //text : []
+//     console.log("submit text", text); //undefined
+//   };
 
-        지금까지 추가하였던 목록 배열(arr)이 
-        <ul>의 li의 항목으로 추가되어야합니다.
+//   // const onSubmit = () => {
+//   //   return arr.current.map((item) => <li key={Math.random()}>{item}</li>)
+//   // }
 
-        만약 제출되었을 때 아무런 항목이 없다면
-        <p>제출된 목록이 없습니다</p>이 노출되어야하며
+//   //2-2
+//   const targetText = useRef();
+//   const onClickChangeColor = () => {
+//     targetText.current.style.color = "red";
+//   };
+//   return (
+//     <>
+//       <h1>문제2</h1>
+//       <div>
+//         <h2>문제 2-1</h2>
+//         <p>
+//           <input ref={Input} />
+//         </p>
+//         <p>
+//           <button onClick={onAddList}>추가</button>
+//         </p>
+//         <p>
+//           <button onClick={onSubmit}>제출</button>
+//         </p>
 
-        제출된 항목이 있다면
-        <ul>만 노출되어야 합니다
+//         {arr.current.length === 0 && text && <p>제출된 목록이 없습니다</p>}
+//         {arr.current.length && <ul>{text}</ul>}
+//       </div>
+//       <div>
+//         <h2>문제 2-2</h2>
+//         <p ref={targetText}> 이 문구는 아래 버튼을 누르면 색상이 바뀝니다</p>
+//         <button onClick={onClickChangeColor}>변경</button>
+//       </div>
+//     </>
+//   );
+// }
+// export default Q2;
 
-        이를 useRef의 특성을 고려하여 풀이해보세요 :)
+// /*
+//     문제2
 
-    2-2)
-        문제 2-2는 변경 버튼을 클릭하면
-        p태그의 색상이 다른 색상으로 변경됩니다.
-        
-        이는 state를 사용하는 것이 가장 올바른 방법이지만
-        어를 사용할 수 없는 어쩔 수 없는 상황에 놓여있습니다.
+//     2-1)
+//         useRef에 관련한 문제입니다.
 
-        따라서 useRef는 사용하여 해당 문구의 색상을 변경해보세요 :)
-  */
+//         추가 버튼을 누르면 input에 있던 value는 배열 arr에 추가됩니다.
+//         그러나, 추가 버튼을 누를 때마다 강제 랜더링 상태가 무조건 적으로 업데이트 됩니다.
+
+//         이러한 상황에서 제출버튼을 누르면
+
+//         지금까지 추가하였던 목록 배열(arr)이
+//         <ul>의 li의 항목으로 추가되어야합니다.
+
+//         만약 제출되었을 때 아무런 항목이 없다면
+//         <p>제출된 목록이 없습니다</p>이 노출되어야하며
+
+//         제출된 항목이 있다면
+//         <ul>만 노출되어야 합니다
+
+//         이를 useRef의 특성을 고려하여 풀이해보세요 :)
+
+//     2-2)
+//         문제 2-2는 변경 버튼을 클릭하면
+//         p태그의 색상이 다른 색상으로 변경됩니다.
+
+//         이는 state를 사용하는 것이 가장 올바른 방법이지만
+//         어를 사용할 수 없는 어쩔 수 없는 상황에 놓여있습니다.
+
+//         따라서 useRef는 사용하여 해당 문구의 색상을 변경해보세요 :)
+//   */
